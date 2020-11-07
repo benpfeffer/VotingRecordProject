b7811fa306f7ea6837dd145964d0e85479f8216f
Salvador Balkus <salbalkus@gmail.com>
2020-11-06 21:19:12 -0500

    added title

--- src/voting/VotingRecordDB.java
+++ src/voting/VotingRecordDB.java
@@ -1,18 +1,27 @@
 package voting;
 
 import javax.swing.*;  
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
 
 public class VotingRecordDB {
 	public static void main(String[] args) {
 		JFrame f=new JFrame();//creating instance of JFrame  
-        
+		//example button
 		JButton b=new JButton("click");//creating instance of JButton  
-		b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+		b.setBounds(130,100,100,40);//x axis, y axis, width, height 
+		//add title
+		JLabel title = new JLabel("Congressional Voting Database", JLabel.CENTER);
+		title.setBounds(40,10,880,80);
+		title.setFont(new Font("Sans-serif", Font.PLAIN, 48));
+		title.setOpaque(true);
+	    title.setBackground(new Color(255, 255, 255));
 		          
 		f.add(b);//adding button in JFrame  
-		          
-		f.setSize(400,500);//400 width and 500 height  
+		f.add(title); //adding title
+		f.setSize(960,720);//960 width and 720 height  
 		f.setLayout(null);//using no layout managers  
 		f.setVisible(true);//making the frame visible  
 	}
